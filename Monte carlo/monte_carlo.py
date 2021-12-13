06eb3389afdbdb1ba0e20ac57082b862d0910aac
Alex Reinoso <ralexjessiel@gmail.com>
2021-12-13 10:23:34 -0500

    update

--- Monte carlo/monte_carlo.py
+++ Monte carlo/monte_carlo.py
@@ -1,19 +1,13 @@
+
 import random
-
-n = 1000000
-r = 1.0
-a, b = (0.0, 0.0)
-x_neg, x_pos = a - r, a + r
-y_neg, y_pos = b - r, b + r
-
-count = 0.0
-for i in range(0, n):
-
-    x = random.uniform(x_neg, x_pos)
-    y = random.uniform(y_neg, y_pos)
-    
-    if x*x + y*y <= 1.0:
-        count += 1
-
-result = (count / float(n)) * 4
-print ("result is ",result)
+ 
+def monteCarlo(n=1000):
+    k = 0
+    for i in range(n):
+        x = random.random()
+        y = random.random()
+        if (x ** 2 + y ** 2 <= 1):
+            k = k + 1
+    return (4 * float(k) / n)
+ 
+print (monteCarlo())
